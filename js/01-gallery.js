669686728a2edf62203a6ef53acd5a982dbf436b
Vitalykin97 <Vitalykin97@gmail.com>
2023-11-14 20:02:54 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+  `);
+
+  instance.show();
+});
