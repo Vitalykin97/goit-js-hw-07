de4c2ab2437409b53360e646ef6603ed809e6537
Vitalykin97 <Vitalykin97@gmail.com>
2023-11-15 18:04:38 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,44 +1,4 @@
-import * as basicLightbox from "basiclightbox";
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
 
-// Приклад: URL зображення
-const imageUrl = "https://example.com/image.jpg";
-
-const instance = basicLightbox.create(`<img src="${imageUrl}" />`);
-
-instance.show();
-
-const gallery = document.querySelector(".gallery");
-
-function createGalleryMarkup(items) {
-  return items
-    .map(
-      ({ preview, original, description }) => `
-      <li class="gallery__item">
-        <a class="gallery__link" href='https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>`
-    )
-    .join("");
-}
-
-gallery.innerHTML = createGalleryMarkup(galleryItems);
-
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" alt="${event.target.alt}">
-  `);
-
-  instance.show();
-});
+console.log(galleryItems);
