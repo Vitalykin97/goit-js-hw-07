cf91b5975c04ceaf1894fa69d73bf0c9eea631e5
Vitalykin97 <Vitalykin97@gmail.com>
2023-11-15 18:16:28 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,14 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+import * as basicLightbox from "basiclightbox";
+
+// Припустимо, що у вас є змінна, яка містить URL великого зображення
+const imageUrl = "https://example.com/large-image.jpg";
+
+// Створення інстанції модального вікна basicLightbox
+const instance = basicLightbox.create(`<img src="${imageUrl}">`);
+
+// Показ модального вікна
+instance.show();
