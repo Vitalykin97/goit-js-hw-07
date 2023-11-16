b0d833037343ec455e64d33f458dcc496e2d3020
Vitalykin97 <Vitalykin97@gmail.com>
2023-11-16 10:26:46 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   const gallery = new SimpleLightbox(".gallery a");
 });
+
+document.getElementById('galleryContainer').insertAdjacentHTML('beforeend', galleryItems);
