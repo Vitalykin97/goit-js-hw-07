50600dfe015a7b8915059c721f0f37e997b5fe10
Vitalykin97 <Vitalykin97@gmail.com>
2023-11-16 09:40:35 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,5 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
+<script>
+document.addEventListener('DOMContentLoaded', function(){
+  const gallery = new SimpleLightbox('.gallery a');
+});
+</script>
